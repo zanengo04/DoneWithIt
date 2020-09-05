0cd5b24feffe0dfe4822ec5591cb560c3a340a5a
Quoc Ngo <zanengo04@gmail.com>
2020-09-05 14:51:20 -0400

    login and register button added

--- App.js
+++ App.js
@@ -6,15 +6,7 @@ import { View } from 'react-native';
 export default function App() {
   return (
     // <ViewImageScreen/>
-    // <WelcomeScreen />
-    <View
-    style ={{
-      flex: 1,
-      justifyContent:'center',
-      alignItems: 'center'
-    }}>
-      <AppButton title ='Login' onPress={() =>console.log('Tapped')}/>
-    </View>
+    <WelcomeScreen />
     
   );
 }
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import { ImageBackground, StyleSheet, View, Image, Text } from 'react-native'
 
+import AppButton from '../components/AppButton'
 export default function WelcomeScreen(props) {
     return (
         <ImageBackground
+        blurRadius={2}
         style = {styles.background} 
         source = {require('../assets/background.jpg')}>
             <View style ={styles.logoContainer}>
                 <Image style = {styles.logo} source={require('../assets/logo-red.png')}></Image>
-                <Text>Sell what you don't need</Text>
+                <Text style ={styles.tagLine}>Sell what you don't need</Text>
             </View>
-            <View style ={styles.loginButton}></View>
-            <View style ={styles.registerButton}></View>
+            <View style = {styles.buttonContainer}>
+                <AppButton title ='Login'/>
+                <AppButton title ='Register' color = 'secondary'/>
+            </View>
         </ImageBackground>
     )
 }
@@ -31,15 +35,14 @@ const styles = StyleSheet.create({
         top:70,
         alignItems :'center'
     },
-    loginButton:{
-        width:'100%',
-        height: 70,
-        backgroundColor: "#fc5c65"
+    buttonContainer:{
+        padding:5,
+        width:'100%'
     },
-    registerButton:{
-        width:'100%',
-        height: 70,
-        backgroundColor: "#4ecdc4"
+    tagLine:{
+        fontSize: 25,
+        fontWeight: "bold",
+        paddingVertical: 20,
     }
     
 })
