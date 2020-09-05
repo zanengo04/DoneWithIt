f4e30d6361769acb20e853b4fa577ff34fa43ff7
Quoc Ngo <zanengo04@gmail.com>
2020-09-05 14:13:23 -0400

    custom button added

--- App.js
+++ App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
+import AppButton from './app/components/AppButton';
+import { View } from 'react-native';
 export default function App() {
   return (
-    <WelcomeScreen />
-    
+    // <ViewImageScreen/>
+    // <WelcomeScreen />
+    <View
+    style ={{
+      flex: 1,
+      justifyContent:'center',
+      alignItems: 'center'
+    }}>
+      <AppButton title ='Login' onPress={() =>console.log('Tapped')}/>
+    </View>
     
   );
 }
---
+++ app/screens/ViewImageScreen.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Image, StyleSheet, View } from 'react-native'
+
+import colors from '../config/colors'
+
+export default function ViewImageScreen() {
+    return (
+        <View style={styles.container}>
+            <View style ={styles.closeIcon}></View>
+            <View style ={styles.deleteIcon}></View>
+            <Image resizeMode ='contain'
+            style={styles.image} source={require('../assets/chair.jpg')}/>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor: "#000",
+        flex:1
+    },
+    closeIcon: {
+        width: 50,
+        height: 50,
+        backgroundColor: colors.primary,
+        position: 'absolute',
+        top: 40,
+        left: 30,
+    },
+    deleteIcon: {
+        width: 50,
+        height: 50,
+        backgroundColor: colors.secondary,
+        position: 'absolute',
+        top: 40,
+        right: 30,
+    },
+    image:{
+        width: '100%',
+        height: '100%'
+    }
+})
