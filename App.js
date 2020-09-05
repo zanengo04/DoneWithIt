bd133949e0c4f15b9a15a069a700a376ec5ceddc
Quoc Ngo <zanengo04@gmail.com>
2020-09-05 15:40:11 -0400

    card component added

--- App.js
+++ App.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import { View } from 'react-native';
+
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import AppButton from './app/components/AppButton';
-import { View } from 'react-native';
+import AppText from './app/components/AppText';
+import Card from './app/components/Card';
 export default function App() {
   return (
     // <ViewImageScreen/>
-    <WelcomeScreen />
+    // <WelcomeScreen />
     
+    <View style ={{
+      backgroundColor: '#f8f4f4',
+      padding: 20,
+      paddingTop: 100,
+
+    }}>
+      <Card 
+        title = "Red jacket for sale"
+        subtitle ="$100"
+        image = {require('./app/assets/jacket.jpg')}
+      />
+    </View>
   );
 }
 
